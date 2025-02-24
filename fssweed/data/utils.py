3cd4b70cbdc866f1d5649fcb6b0b8e59633c3591
pasqualedem <pasquale199876@hotmail.it>
2025-02-24 10:15:15 +0100

    fix

--- fssweed/data/utils.py
+++ fssweed/data/utils.py
@@ -663,11 +663,11 @@ def merge_dicts(prompts, imgs):
             if k == BatchKeys.IMAGES:
                 merge_prompts[k] = merge_prompts[k].unsqueeze(dim=0)
                 dim = 1
-                out[k] = torch.cat([imgs[k].cpu(), merge_prompts[k].cpu()], dim=dim).to(
+                out[k] = torch.cat([imgs[k].to(device), merge_prompts[k].to(device)], dim=dim).to(
                     device
                 )
             if k == BatchKeys.DIMS:
-                out[k] = torch.cat([imgs[k].cuda(), prompts[k]], dim=0).unsqueeze(dim=0)
+                out[k] = torch.cat([imgs[k].to(device), prompts[k].to(device)], dim=0).unsqueeze(dim=0)
             if k == BatchKeys.IMAGE_IDS:
                 out[k] = [*imgs[k], *merge_prompts[k]]
         elif k in imgs:
