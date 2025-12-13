6680bd6ba2d7b399c80e49e504f26e5e7330f9fd
pasqualedem <pasquale199876@hotmail.it>
2025-12-13 11:24:52 +0100

    move dcama scripts

--- dcama/test.sh
+++ dcama/test.sh
@@ -1,12 +1,12 @@
-python ./test_dcama.py --datapath "../PANet/data/Pascal/VOCdevkit" \
+python ./test_dcama.py --datapath "./data/Pascal/VOCdevkit" \
                  --benchmark pascal \
                  --fold 0 \
                  --bsz 1 \
                  --nworker 8 \
                  --backbone swin \
-                 --feature_extractor_path "../LabelAnything/checkpoints/swin_base_patch4_window12_384.pth" \
+                 --feature_extractor_path "./checkpoints/swin_base_patch4_window12_384.pth" \
                  --logpath "./logs" \
-                 --load "../LabelAnything/checkpoints/dcama/pascal/fold0_dcama_swin_xm0xgeoo.safetensors" \
+                 --load "./checkpoints/dcama/pascal/fold0_dcama_swin_xm0xgeoo.safetensors" \
                  --nshot 1 \
                  --vispath "./vis_1" \
                 #  --visualize
